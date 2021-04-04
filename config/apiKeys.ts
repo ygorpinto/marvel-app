e2202c0c7ada377d5c9d93619023c09e2af4d8ad
Ygor Ferreira <ygor.pinto@hotmail.com>
2021-04-04 04:57:36 -0300

    ok

---
+++ config/apiKeys.ts
@@ -0,0 +1,4 @@
+export default {
+        API_KEY:"2e092906e367454ab7e5fcf261b0881b",
+        HASH:"94d69e6b0ad19dc67e01ad5184f4ffc5"
+};
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,55 @@
 import Container from "../components/Container";
-import GlobalStyles from "../styles/global";
+import keys from '../../config/apiKeys'
+import { useEffect, useState } from "react";
+
+const API_KEY = keys.API_KEY
+const HASH = keys.HASH
 
 export default function Home() {
+  
+  const [result,setResult] = useState(undefined);
+  const [offset,setOffset] = useState(0);
+  
+  const api = `https://gateway.marvel.com/v1/public/characters?ts=1&apikey=${API_KEY}&hash=${HASH}&offset=${offset}`
+  
+  useEffect(()=>{
+    fetchData()
+  },[offset])
+  
+  const nextPage = () => {
+    setOffset(offset+20)
+  }
+  const prevPage = () => {
+    setOffset(offset-20)
+  }
+  
+  const fetchData = async () => {
+    const res = await fetch(api)
+    const data = await res.json()
+    setResult(data);
+  }
+  
   return (
   <>
-    <GlobalStyles />
       <Container>
-        
+        {typeof result !== "undefined" ? result.data.results.map(item => (
+          <div key={item.name}>
+            <img src={`${item.path}`}/>
+            <p>{item.name}</p>
+          </div>
+        )):("")}
+        {offset === 0 ? <button
+        onClick={nextPage}
+        >Proxima</button> : (
+          <>
+        <button
+        onClick={prevPage}
+        >Anterior</button>
+          <button
+        onClick={nextPage}
+        >Proxima</button>
+        </>
+        )}
       </Container>
   </>
   )
