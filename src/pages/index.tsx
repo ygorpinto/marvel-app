41476b8f4028ad3485ef166234be4c1e0d3eb3c8
Ygor Ferreira <ygor.pinto@hotmail.com>
2021-04-13 14:50:10 -0300

    ok

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,27 +2,14 @@ import HeroesList from "../components/HeroesList/Heroeslist";
 import Main from "../components/Main/Main";
 import MainContainer from "../components/MainContainer/MainContainer";
 import React from "react";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-} from "react-router-dom";
 
 export default function Home() {
 
   return (
     <>
-      <Router>
         <MainContainer>
-          <Switch>
-            <Route path="/" exact>
-            </Route>
-            <Route path="/heroes">
               <HeroesList />
-            </Route>
-          </Switch>
         </MainContainer>
-      </Router>
     </>
   )
 }
